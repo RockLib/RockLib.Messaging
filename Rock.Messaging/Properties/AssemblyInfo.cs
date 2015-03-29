c616a09b3ed9f0004d62cda2df33f54a5e40ec04
Brian Friesen <brianfriesen@quickenloans.com>
2015-03-29 16:22:25 -0400

    Update nuget packages.

--- Rock.Messaging/Properties/AssemblyInfo.cs
+++ Rock.Messaging/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.0.0")]
 [assembly: AssemblyFileVersion("0.0.2")]
-[assembly: AssemblyInformationalVersion("0.0.2-alpha04")]
+[assembly: AssemblyInformationalVersion("0.0.2-alpha05")]
