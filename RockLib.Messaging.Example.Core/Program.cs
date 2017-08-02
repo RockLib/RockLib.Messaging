f11301777d61f01d2ecb3a68ffb8526344d0c3be
derikwhittaker@quickenloans.com <derik.whittaker@gmail.com>
2017-08-02 14:49:31 -0400

    shell example app for core

---
+++ RockLib.Messaging.Example.Core/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RockLib.Messaging.Example.Core
+{
+    class Program
+    {
+        // using project reference for now, will change to nuget once we get Messaging Stable
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
