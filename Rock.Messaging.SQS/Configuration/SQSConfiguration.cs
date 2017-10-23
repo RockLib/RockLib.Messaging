e04ef7531c4c3fe72f25b5a77e77910a168773d3
Justin Moss <Justin.Moss@gmail.com>
2017-10-23 09:36:48 -0400

    Add simple SQS Configuration classes for RockLib

---
+++ Rock.Messaging.SQS/Configuration/SQSConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RockLib.Messaging.SQS.Configuration
+{
+    public class SQSConfiguration: ISQSConfiguration
+    {
+        public SQSConfiguration()
+        {
+            MaxMessages = 3;
+            AutoAcknowledge = true;
+        }
+
+        public String Name { get; set; }
+        public String QueueUrl { get; set; }
+        public Int32 MaxMessages { get; set; }
+        public Boolean AutoAcknowledge { get; set; }
+        public Boolean Compressed { get; set; }
+        public void Validate()
+        {
+            if (Name == null)
+            {
+                throw new Exception("Name must be set.");
+            }
+
+            if (QueueUrl == null)
+            {
+                throw new Exception("QueueUrl must be set.");
+            }
+
+            if (MaxMessages < 1)
+            {
+                throw new Exception("MaxMessages must be greater than zero.");
+            }
+        }
+    }
+}
---
+++ Rock.Messaging.SQS/Configuration/SQSConfigurationProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace RockLib.Messaging.SQS.Configuration
+{
+    public class SQSConfigurationProvider: ISQSConfigurationProvider
+    {
+        private ISQSConfiguration[] _configurations = new ISQSConfiguration[0];
+        
+        public ISQSConfiguration[] Configurations
+        {
+            get => _configurations;
+            set => _configurations = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public ISQSConfiguration GetConfiguration(string name)
+        {
+            return _configurations.First(c => c.Name == name);
+        }
+
+        public bool HasConfiguration(string name)
+        {
+            return _configurations.Any(c => c.Name == name);
+        }
+
+        public void Validate()
+        {
+            foreach (var configuration in _configurations)
+            {
+                configuration.Validate();
+            }
+
+            if (_configurations.Select(c => c.Name).Distinct().Count() != _configurations.Length)
+            {
+                throw new Exception("Each configuration Name must be unique.");
+            }
+        }
+    }
+}
