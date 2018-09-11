b85e328452c5553d138890c217f53bcd68233945
Brian Friesen <brianfriesen@quickenloans.com>
2018-09-11 09:01:57 -0400

    Make sync-over-async sender extensions safe, refactor

--- RockLib.Messaging/SenderExtensions.cs
+++ RockLib.Messaging/SenderExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace RockLib.Messaging
 {
@@ -13,49 +15,53 @@ namespace RockLib.Messaging
         /// </summary>
         /// <param name="source">The <see cref="ISender"/> from which to send the message.</param>
         /// <param name="message">The message to send.</param>
-        public static void Send(this ISender source, SenderMessage message)
-        {
-            source.SendAsync(message).Wait();
-        }
+        public static void Send(this ISender source, SenderMessage message) =>
+            source.SendSync(s => s.SendAsync(message));
 
         /// <summary>
         /// Synchronously sends the specified string message.
         /// </summary>
         /// <param name="source">The <see cref="ISender"/> from which to send the message.</param>
         /// <param name="message">The message to send.</param>
-        public static void Send(this ISender source, string message)
-        {
-            source.SendAsync(message).Wait();
-        }
+        public static void Send(this ISender source, string message) =>
+            source.SendSync(s => s.SendAsync(message));
 
         /// <summary>
         /// Asynchronously sends the specified string message.
         /// </summary>
         /// <param name="source">The <see cref="ISender"/> from which to send the message.</param>
         /// <param name="message">The message to send.</param>
-        public static Task SendAsync(this ISender source, string message)
-        {
-            return source.SendAsync(new SenderMessage(message));
-        }
+        public static Task SendAsync(this ISender source, string message) =>
+            source.SendAsync(new SenderMessage(message));
 
         /// <summary>
         /// Synchronously sends the specified binary message.
         /// </summary>
         /// <param name="source">The <see cref="ISender"/> from which to send the message.</param>
         /// <param name="message">The message to send.</param>
-        public static void Send(this ISender source, byte[] message)
-        {
-            source.SendAsync(message).Wait();
-        }
+        public static void Send(this ISender source, byte[] message) =>
+            source.SendSync(s => s.SendAsync(message));
 
         /// <summary>
         /// Asynchronously sends the specified binary message.
         /// </summary>
         /// <param name="source">The <see cref="ISender"/> from which to send the message.</param>
         /// <param name="message">The message to send.</param>
-        public static Task SendAsync(this ISender source, byte[] message)
+        public static Task SendAsync(this ISender source, byte[] message) =>
+            source.SendAsync(new SenderMessage(message));
+
+        private static void SendSync(this ISender source, Func<ISender, Task> sendAsync)
         {
-            return source.SendAsync(new SenderMessage(message));
+            SynchronizationContext old = SynchronizationContext.Current;
+            try
+            {
+                SynchronizationContext.SetSynchronizationContext(null);
+                sendAsync(source).GetAwaiter().GetResult();
+            }
+            finally
+            {
+                SynchronizationContext.SetSynchronizationContext(old);
+            }
         }
     }
 }
