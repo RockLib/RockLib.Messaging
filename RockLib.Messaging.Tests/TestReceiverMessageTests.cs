77ffe2f5ef2dbfcd76f2faf536f5fc031c73f673
Brian Friesen <brianfriesen@quickenloans.com>
2018-12-04 21:23:59 -0500

    Add TestReceiverMessage class

---
+++ RockLib.Messaging.Tests/TestReceiverMessageTests.cs
@@ -0,0 +1,189 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RockLib.Messaging.Testing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockLib.Messaging.Tests
+{
+    [TestFixture]
+    public class TestReceiverMessageTests
+    {
+        [Test]
+        public void StringPayloadIsSetFromStringPayload()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.StringPayload.Should().BeSameAs("Hello, world!");
+        }
+
+        [Test]
+        public void BinaryPayloadIsSetFromUTF8EncodedStringPayload()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.BinaryPayload.Should().BeEquivalentTo(Encoding.UTF8.GetBytes("Hello, world!"), x => x.WithStrictOrdering());
+        }
+
+        [Test]
+        public void BinaryPayloadIsSetFromBinaryPayload()
+        {
+            var payload = new byte[] { 1, 2, 3, 4, 5 };
+
+            var message = new TestReceiverMessage(payload);
+
+            message.BinaryPayload.Should().BeSameAs(payload);
+        }
+
+        [Test]
+        public void StringPayloadIsSetFromBase64EncodedBinaryPayload()
+        {
+            var payload = new byte[] { 1, 2, 3, 4, 5 };
+
+            var message = new TestReceiverMessage(payload);
+
+            message.StringPayload.Should().Be(Convert.ToBase64String(payload));
+        }
+
+        [Test]
+        public void PublicHeaderPropertyIsNotTheSameAsExplicitInterfaceHeaderProperty()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            ((object)message.Headers).Should().NotBeSameAs(((IReceiverMessage)message).Headers);
+        }
+
+        [Test]
+        public void PublicHeaderPropertyHasSameContentsAsExplicitInterfaceHeaderProperty()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Headers.Add("foo", "abc");
+            message.Headers.Add("bar", 123);
+
+            HeaderDictionary interfaceHeaders = ((IReceiverMessage)message).Headers;
+
+            interfaceHeaders.ContainsKey("foo");
+            interfaceHeaders["foo"].Should().Be("abc");
+
+            interfaceHeaders.ContainsKey("bar");
+            interfaceHeaders["bar"].Should().Be(123);
+        }
+
+        [Test]
+        public void HandledIsFalseBeforeMessageIsHandled()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Handled.Should().BeFalse();
+        }
+
+        [Test]
+        public void HandledByIsNullBeforeMessageIsHandled()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.HandledBy.Should().BeNull();
+        }
+
+        [Test]
+        public void HandledIsTrueAfterAcknowledgeIsCalled()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Acknowledge();
+
+            message.Handled.Should().BeTrue();
+        }
+
+        [Test]
+        public void HandledByIsAcknowledgeAfterAcknowledgeIsCalled()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Acknowledge();
+
+            message.HandledBy.Should().Be(nameof(message.Acknowledge));
+        }
+
+        [Test]
+        public void HandledIsTrueAfterRollbackIsCalled()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Rollback();
+
+            message.Handled.Should().BeTrue();
+        }
+
+        [Test]
+        public void HandledByIsRollbackAfterRollbackIsCalled()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Rollback();
+
+            message.HandledBy.Should().Be(nameof(message.Rollback));
+        }
+
+        [Test]
+        public void HandledIsTrueAfterRejectIsCalled()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Reject();
+
+            message.Handled.Should().BeTrue();
+        }
+
+        [Test]
+        public void HandledByIsRejectAfterRejectIsCalled()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Reject();
+
+            message.HandledBy.Should().Be(nameof(message.Reject));
+        }
+
+        [Test]
+        public void AcknowledgeThrowsIfHandledIsTrue()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Acknowledge();
+
+            Action act = () => message.Acknowledge();
+
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage($"Cannot {nameof(message.Acknowledge)} message: the message has already been handled by {nameof(message.Acknowledge)}.");
+        }
+
+        [Test]
+        public void RollbackThrowsIfHandledIsTrue()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Acknowledge();
+
+            Action act = () => message.Rollback();
+
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage($"Cannot {nameof(message.Rollback)} message: the message has already been handled by {nameof(message.Acknowledge)}.");
+        }
+
+        [Test]
+        public void RejectThrowsIfHandledIsTrue()
+        {
+            var message = new TestReceiverMessage("Hello, world!");
+
+            message.Acknowledge();
+
+            Action act = () => message.Reject();
+
+            act.Should().Throw<InvalidOperationException>()
+                .WithMessage($"Cannot {nameof(message.Reject)} message: the message has already been handled by {nameof(message.Acknowledge)}.");
+        }
+    }
+}
---
+++ RockLib.Messaging/Testing/TestReceiverMessage.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace RockLib.Messaging.Testing
+{
+    /// <summary>
+    /// An implementation of <see cref="IReceiverMessage"/> that makes testing
+    /// <see cref="IReceiver"/> message handlers easier.
+    /// </summary>
+    public class TestReceiverMessage : IReceiverMessage
+    {
+        private readonly HeaderDictionary _headerDictionary;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestReceiverMessage"/> class.
+        /// </summary>
+        /// <param name="payload">The payload of the test message.</param>
+        public TestReceiverMessage(string payload)
+            : this()
+        {
+            StringPayload = payload ?? throw new ArgumentNullException(nameof(payload));
+            BinaryPayload = Encoding.UTF8.GetBytes(payload);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestReceiverMessage"/> class.
+        /// </summary>
+        /// <param name="payload">The payload of the test message.</param>
+        public TestReceiverMessage(byte[] payload)
+            : this()
+        {
+            BinaryPayload = payload ?? throw new ArgumentNullException(nameof(payload));
+            StringPayload = Convert.ToBase64String(payload);
+        }
+
+        private TestReceiverMessage()
+        {
+            var headers = new Dictionary<string, object>();
+            Headers = headers;
+            _headerDictionary = new HeaderDictionary(headers);
+        }
+
+        /// <inheritdoc />
+        public string StringPayload { get; }
+
+        /// <inheritdoc />
+        public byte[] BinaryPayload { get; }
+
+        /// <summary>
+        /// Gets the headers of the test message.
+        /// </summary>
+        public IDictionary<string, object> Headers { get; }
+
+        HeaderDictionary IReceiverMessage.Headers => _headerDictionary;
+
+        /// <inheritdoc />
+        public bool Handled => HandledBy != null;
+
+        /// <summary>
+        /// Gets the name of the method (Acknowledge, Rollback, or Reject) that
+        /// handled the test message.
+        /// </summary>
+        public string HandledBy { get; private set; }
+
+        /// <inheritdoc />
+        public void Acknowledge()
+        {
+            lock (this)
+            {
+                ThrowIfHandled();
+                SetHandled();
+            }
+        }
+
+        /// <inheritdoc />
+        public void Rollback()
+        {
+            lock (this)
+            {
+                ThrowIfHandled();
+                SetHandled();
+            }
+        }
+
+        /// <inheritdoc />
+        public void Reject()
+        {
+            lock (this)
+            {
+                ThrowIfHandled();
+                SetHandled();
+            }
+        }
+
+        private void ThrowIfHandled([CallerMemberName] string callerMemberName = null)
+        {
+            if (Handled)
+                throw new InvalidOperationException($"Cannot {callerMemberName} message: the message has already been handled by {HandledBy}.");
+        }
+
+        private void SetHandled([CallerMemberName] string callerMemberName = null)
+        {
+            HandledBy = callerMemberName;
+        }
+    }
+}
