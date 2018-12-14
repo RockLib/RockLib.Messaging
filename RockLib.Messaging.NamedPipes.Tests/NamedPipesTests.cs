2b1cfbe2606d6dcb923a2c8501c8a7e0779de58a
Brian Friesen <brianfriesen@quickenloans.com>
2018-12-13 21:45:50 -0500

    Include header in named pipe test

--- RockLib.Messaging.NamedPipes.Tests/NamedPipesTests.cs
+++ RockLib.Messaging.NamedPipes.Tests/NamedPipesTests.cs
@@ -13,22 +13,25 @@ namespace RockLib.Messaging.NamedPipes.Tests
             using (var receiver = new NamedPipeReceiver("foo", "test-pipe"))
             {
                 string payload = null;
+                string headerValue = null;
 
                 receiver.Start(m =>
                 {
                     payload = m.StringPayload;
+                    headerValue = m.Headers.GetValue<string>("bar");
                     m.Acknowledge();
                     waitHandle.Set();
                 });
 
                 using (var sender = new NamedPipeSender("foo", "test-pipe"))
                 {
-                    sender.Send("Hello, world!");
+                    sender.Send(new SenderMessage("Hello, world!") { Headers = { { "bar", "abc" } } });
                 }
 
                 waitHandle.WaitOne();
 
                 Assert.Equal("Hello, world!", payload);
+                Assert.Equal("abc", headerValue);
             }
         }
     }
