82dabae1143cecc104ce06c48fa39d34e43c39e5
Brian Friesen <brian.friesen@gmail.com>
2020-03-03 12:07:00 -0500

    Add DI extensions for SNS project

---
+++ RockLib.Messaging.SNS/DependencyInjection/SNSExtensions.cs
@@ -0,0 +1,34 @@
+﻿#if !NET451
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RockLib.Messaging.SNS;
+using System;
+
+namespace RockLib.Messaging.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for registering SNS senders.
+    /// </summary>
+    public static class SNSExtensions
+    {
+        /// <summary>
+        /// Adds an <see cref="SNSSender"/> to the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the sender.</param>
+        /// <param name="configureOptions">A callback for configuring the <see cref="SNSSenderOptions"/>.</param>
+        /// <returns>A builder allowing the sender to be decorated.</returns>
+        public static ISenderBuilder AddSNSSender(this IServiceCollection services, string name, Action<SNSSenderOptions> configureOptions = null)
+        {
+            return services.AddSender(serviceProvider =>
+            {
+                var optionsMonitor = serviceProvider.GetService<IOptionsMonitor<SNSSenderOptions>>();
+                var options = optionsMonitor?.Get(name) ?? new SNSSenderOptions();
+                configureOptions?.Invoke(options);
+
+                return new SNSSender(name, options.TopicArn, options.Region);
+            });
+        }
+    }
+}
+#endif
---
+++ RockLib.Messaging.SNS/DependencyInjection/SNSSenderOptions.cs
@@ -0,0 +1,29 @@
+﻿#if !NET451
+using RockLib.Messaging.SNS;
+using System;
+
+namespace RockLib.Messaging.DependencyInjection
+{
+    /// <summary>
+    /// Defines the settings for creating instances of <see cref="SNSSender"/>.
+    /// </summary>
+    public class SNSSenderOptions
+    {
+        private string _topicArn;
+
+        /// <summary>
+        /// Gets or sets the arn of the SNS topic.
+        /// </summary>
+        public string TopicArn
+        {
+            get => _topicArn;
+            set => _topicArn = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        /// <summary>
+        /// Gets or sets the region of the SNS topic.
+        /// </summary>
+        public string Region { get; set; }
+    }
+}
+#endif
