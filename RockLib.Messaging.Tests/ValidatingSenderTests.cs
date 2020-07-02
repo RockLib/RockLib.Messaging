0c83c1dbabd1af94744fa2ba569cc06ff766fa3f
Brian Friesen <brian.friesen@gmail.com>
2020-07-02 16:43:16 -0400

    Add tests for validation

---
+++ RockLib.Messaging.Tests/ValidatingSenderTests.cs
@@ -0,0 +1,95 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RockLib.Messaging.Tests
+{
+    public class ValidatingSenderTests
+    {
+        [Fact]
+        public void ConstructorHappyPath()
+        {
+            var mockSender = new Mock<ISender>();
+            Action<SenderMessage> validate = message => { };
+
+            var validatingSender = new ValidatingSender("Foo", mockSender.Object, validate);
+
+            validatingSender.Name.Should().Be("Foo");
+            validatingSender.Sender.Should().BeSameAs(mockSender.Object);
+            validatingSender.Validate.Should().BeSameAs(validate);
+        }
+
+        [Fact]
+        public void ConstructorSadPath1()
+        {
+            ISender sender = new Mock<ISender>().Object;
+            Action<SenderMessage> validate = message => { };
+
+            Action act = () => new ValidatingSender(null, sender, validate);
+
+            act.Should().ThrowExactly<ArgumentNullException>().WithMessage("*name*");
+        }
+
+        [Fact]
+        public void ConstructorSadPath2()
+        {
+            ISender sender = null;
+            Action<SenderMessage> validate = message => { };
+
+            Action act = () => new ValidatingSender("Foo", sender, validate);
+
+            act.Should().ThrowExactly<ArgumentNullException>().WithMessage("*sender*");
+        }
+
+        [Fact]
+        public void ConstructorSadPath3()
+        {
+            ISender sender = new Mock<ISender>().Object;
+            Action<SenderMessage> validate = null;
+
+            Action act = () => new ValidatingSender("Foo", sender, validate);
+
+            act.Should().ThrowExactly<ArgumentNullException>().WithMessage("*validate*");
+        }
+
+        [Fact]
+        public void DisposeMethodHappyPath()
+        {
+            var mockSender = new Mock<ISender>();
+            Action<SenderMessage> validate = message => { };
+
+            var validatingSender = new ValidatingSender("Foo", mockSender.Object, validate);
+
+            validatingSender.Dispose();
+
+            mockSender.Verify(m => m.Dispose(), Times.Once());
+        }
+
+        [Fact]
+        public async Task SendAsyncMethodHappyPath()
+        {
+            var sentMessages = new List<SenderMessage>();
+
+            var mockSender = new Mock<ISender>();
+            Action<SenderMessage> validate = message => sentMessages.Add(message);
+
+            var validatingSender = new ValidatingSender("Foo", mockSender.Object, validate);
+
+            var message1 = new SenderMessage("Hello, world!");
+            var message2 = new SenderMessage("Good-bye, cruel world!");
+
+            await validatingSender.SendAsync(message1);
+            await validatingSender.SendAsync(message2);
+
+            mockSender.Verify(m => m.SendAsync(message1, default), Times.Once());
+            mockSender.Verify(m => m.SendAsync(message2, default), Times.Once());
+
+            sentMessages.Should().HaveCount(2);
+            sentMessages[0].Should().Be(message1);
+            sentMessages[1].Should().Be(message2);
+        }
+    }
+}
---
+++ RockLib.Messaging.Tests/ValidatingTransactionalSenderTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using Xunit;
+
+namespace RockLib.Messaging.Tests
+{
+    public class ValidatingTransactionalSenderTests
+    {
+        [Fact]
+        public void ConstructorHappyPath()
+        {
+            var transactionalSender = new Mock<ITransactionalSender>().Object;
+            Action<SenderMessage> validate = message => { };
+
+            var validatingTransactionalSender = new ValidatingTransactionalSender("Foo", transactionalSender, validate);
+
+            validatingTransactionalSender.Name.Should().Be("Foo");
+            validatingTransactionalSender.Sender.Should().BeSameAs(transactionalSender);
+            validatingTransactionalSender.TransactionalSender.Should().BeSameAs(transactionalSender);
+            validatingTransactionalSender.Validate.Should().BeSameAs(validate);
+        }
+
+        [Fact]
+        public void ConstructorSadPath1()
+        {
+            ITransactionalSender transactionalSender = new Mock<ITransactionalSender>().Object;
+            Action<SenderMessage> validate = message => { };
+
+            Action act = () => new ValidatingTransactionalSender(null, transactionalSender, validate);
+
+            act.Should().ThrowExactly<ArgumentNullException>().WithMessage("*name*");
+        }
+
+        [Fact]
+        public void ConstructorSadPath2()
+        {
+            ITransactionalSender transactionalSender = null;
+            Action<SenderMessage> validate = message => { };
+
+            Action act = () => new ValidatingTransactionalSender("Foo", transactionalSender, validate);
+
+            act.Should().ThrowExactly<ArgumentNullException>().WithMessage("*sender*");
+        }
+
+        [Fact]
+        public void ConstructorSadPath3()
+        {
+            ITransactionalSender transactionalSender = new Mock<ITransactionalSender>().Object;
+            Action<SenderMessage> validate = null;
+
+            Action act = () => new ValidatingTransactionalSender("Foo", transactionalSender, validate);
+
+            act.Should().ThrowExactly<ArgumentNullException>().WithMessage("*validate*");
+        }
+
+        [Fact]
+        public void BeginTransactionMethodHappyPath()
+        {
+            var senderTransaction = new Mock<ISenderTransaction>().Object;
+
+            var mockTransactionalSender = new Mock<ITransactionalSender>();
+            mockTransactionalSender.Setup(m => m.BeginTransaction()).Returns(senderTransaction);
+            Action<SenderMessage> validate = message => { };
+
+            var validatingTransactionalSender = new ValidatingTransactionalSender("Foo", mockTransactionalSender.Object, validate);
+
+            var actualTransaction = validatingTransactionalSender.BeginTransaction();
+
+            var validatingSenderTransaction =
+                actualTransaction.Should().BeOfType<ValidatingSenderTransaction>().Subject;
+
+            validatingSenderTransaction.Transaction.Should().BeSameAs(senderTransaction);
+            validatingSenderTransaction.Validate.Should().BeSameAs(validate);
+        }
+    }
+}
