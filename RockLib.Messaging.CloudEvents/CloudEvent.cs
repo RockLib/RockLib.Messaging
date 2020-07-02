7178be30473a860a3e45038a5b3588cc429756ab
Brian Friesen <brian.friesen@gmail.com>
2020-07-02 17:02:03 -0400

    Add CloudEvents project

---
+++ RockLib.Messaging.CloudEvents/CloudEvent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace RockLib.Messaging.CloudEvents
+{
+    /// <summary>
+    /// The default CloudEvent.
+    /// </summary>
+    public sealed class CloudEvent : CloudEventBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloudEvent"/> type.
+        /// </summary>
+        public CloudEvent()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloudEvent"/> type.
+        /// </summary>
+        /// <param name="data">The data of the cloud event.</param>
+        public CloudEvent(string data) => SetData(data);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CloudEvent"/> type.
+        /// </summary>
+        /// <param name="data">The data of the cloud event.</param>
+        public CloudEvent(byte[] data) => SetData(data);
+
+        /// <summary>
+        /// Validates that the <paramref name="senderMessage"/> parameter has the correct headers in
+        /// order to represent a CloudEvent. Adds any missing required headers that can be determined
+        /// at runtime.
+        /// </summary>
+        /// <param name="senderMessage"></param>
+        public static void Validate(SenderMessage senderMessage) =>
+            ValidateCore(senderMessage ?? throw new ArgumentNullException(nameof(senderMessage)));
+
+        /// <summary>
+        /// Creates an instance of <see cref="CloudEvent"/> with properties mapped from the headers of
+        /// <paramref name="receiverMessage"/>.
+        /// </summary>
+        /// <param name="receiverMessage">
+        /// The <see cref="IReceiverMessage"/> to be mapped to the new <see cref="CloudEvent"/>.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="CloudEvent"/> with properties mapped from the headers of the <see cref="IReceiverMessage"/>.
+        /// </returns>
+        public static CloudEvent Create(IReceiverMessage receiverMessage) =>
+            Create<CloudEvent>(receiverMessage ?? throw new ArgumentNullException(nameof(receiverMessage)));
+
+        /// <summary>
+        /// Converts the <see cref="CloudEvent"/> to a <see cref="SenderMessage"/> by calling
+        /// <see cref="CloudEventBase.ToSenderMessage"/>.
+        /// </summary>
+        /// <param name="cloudEvent">The <see cref="CloudEvent"/> to convert to a <see cref="SenderMessage"/>.</param>
+        public static implicit operator SenderMessage(CloudEvent cloudEvent) =>
+            cloudEvent?.ToSenderMessage();
+    }
+}
---
+++ RockLib.Messaging.CloudEvents/CloudEventExtensions.cs
@@ -0,0 +1,31 @@
+﻿using RockLib.Messaging.DependencyInjection;
+
+namespace RockLib.Messaging.CloudEvents
+{
+    /// <summary>
+    /// Extension methods related to CloudEvents.
+    /// </summary>
+    public static class CloudEventExtensions
+    {
+        /// <summary>
+        /// Creates an instance of <see cref="CloudEvent"/> with properties mapped from the headers of
+        /// <paramref name="receiverMessage"/>.
+        /// </summary>
+        /// <param name="receiverMessage">
+        /// The <see cref="IReceiverMessage"/> to be mapped to the new <see cref="CloudEvent"/>.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="CloudEvent"/> with properties mapped from the headers of the <see cref="IReceiverMessage"/>.
+        /// </returns>
+        public static CloudEvent ToCloudEvent(this IReceiverMessage receiverMessage) =>
+            CloudEvent.Create(receiverMessage);
+
+        /// <summary>
+        /// Adds a <see cref="ValidatingSender"/> decorator that ensures messages are valid CloudEvents.
+        /// </summary>
+        /// <param name="builder">The <see cref="ISenderBuilder"/>.</param>
+        /// <returns>The same <see cref="ISenderBuilder"/>.</returns>
+        public static ISenderBuilder AddCloudEventValidation(this ISenderBuilder builder) =>
+            builder.AddValidation(CloudEvent.Validate);
+    }
+}
