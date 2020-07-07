b7a15be4eaee706db0daa7562de30459210af42e
Brian Friesen <brian.friesen@gmail.com>
2020-07-07 16:36:28 -0400

    Small improvements

--- RockLib.Messaging.CloudEvents/SequenceTypes.cs
+++ RockLib.Messaging.CloudEvents/SequenceTypes.cs
@@ -1,20 +1,20 @@
 ﻿namespace RockLib.Messaging.CloudEvents
 {
     /// <summary>
-    /// Defines values for the <see cref="SequenceEvent.SequenceType"/> property.
+    /// Defines values for the <see cref="SequenceEvent.SequenceType"/> attribute.
     /// </summary>
     public static class SequenceTypes
     {
         /// <summary>
-        /// If the <see cref="SequenceEvent.SequenceType"/> is set to <see cref="Integer"/>,
-        /// the sequence attribute has the following semantics:
+        /// If the <see cref="SequenceEvent.SequenceType"/> attribute is set to <c>"Integer"</c>,
+        /// the <see cref="SequenceEvent.Sequence"/> attribute has the following semantics:
         /// <list type="bullet">
         /// <item>The values of sequence are string-encoded signed 32-bit Integers.</item>
         /// <item>The sequence MUST start with a value of 1 and increase by 1 for each subsequent value
-        /// (i.e.be contiguous and monotonically increasing).</item>
+        /// (i.e. be contiguous and monotonically increasing).</item>
         /// <item>The sequence wraps around from 2,147,483,647 (2^31 -1) to -2,147,483,648 (-2^31).</item>
         /// </list>
         /// </summary>
-        public static string Integer => "Integer";
+        public const string Integer = "Integer";
     }
 }
