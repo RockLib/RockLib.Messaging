3b6135620d4e04c1d1b4011241f6e46fbb6627a0
Justin Moss <justin.moss@gmail.com>
2020-07-09 14:16:49 -0400

    Add dependency injection extensions for Kafka.

---
+++ RockLib.Messaging.Kafka/DependencyInjection/KafkaExtensions.cs
@@ -0,0 +1,53 @@
+﻿#if !NET451
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using RockLib.Messaging.DependencyInjection;
+using System;
+
+namespace RockLib.Messaging.Kafka.DependencyInjection
+{
+    /// <summary>
+    /// Extension methods for registering Kafka senders and receivers.
+    /// </summary>
+    public static class KafkaExtensions
+    {
+        /// <summary>
+        /// Adds a <see cref="KafkaSender"/> to the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the sender.</param>
+        /// <param name="configureOptions">A callback for configuring the <see cref="KafkaSenderOptions"/>.</param>
+        /// <returns>A builder allowing the sender to be decorated.</returns>
+        public static ISenderBuilder AddKafkaSender(this IServiceCollection services, string name, Action<KafkaSenderOptions> configureOptions = null)
+        {
+            return services.AddSender(serviceProvider =>
+            {
+                var optionsMonitor = serviceProvider.GetService<IOptionsMonitor<KafkaSenderOptions>>();
+                var options = optionsMonitor?.Get(name) ?? new KafkaSenderOptions();
+                configureOptions?.Invoke(options);
+
+                return new KafkaSender(name, options.Topic, options.BootstrapServers, options.MessageTimeoutMs, options.Config);
+            });
+        }
+
+        /// <summary>
+        /// Adds a <see cref="KafkaReceiver"/> to the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the receiver.</param>
+        /// <param name="configureOptions">A callback for configuring the <see cref="KafkaReceiverOptions"/>.</param>
+        /// <returns>A builder allowing the receiver to be decorated.</returns>
+        public static IReceiverBuilder AddKafkaReceiver(this IServiceCollection services, string name, Action<KafkaReceiverOptions> configureOptions = null)
+        {
+            return services.AddReceiver(serviceProvider =>
+            {
+                var optionsMonitor = serviceProvider.GetService<IOptionsMonitor<KafkaReceiverOptions>>();
+                var options = optionsMonitor?.Get(name) ?? new KafkaReceiverOptions();
+                configureOptions?.Invoke(options);
+
+                return new KafkaReceiver(name, options.Topic, options.GroupId, options.BootstrapServers, options.Config);
+            });
+        }
+    }
+}
+#endif
---
+++ RockLib.Messaging.Kafka/DependencyInjection/KafkaReceiverOptions.cs
@@ -0,0 +1,32 @@
+﻿#if !NET451
+using Confluent.Kafka;
+
+namespace RockLib.Messaging.Kafka.DependencyInjection
+{
+    /// <summary>
+    /// Defines the settings for creating instances of <see cref="KafkaReceiver"/>.
+    /// </summary>
+    public class KafkaReceiverOptions
+    {
+        /// <summary>
+        /// Gets or sets the topic to subscribe to.
+        /// </summary>
+        public string Topic { get; set; }
+
+        /// <summary>
+        /// Gets or sets the list of brokers as a CSV list of broker host or host:port.
+        /// </summary>
+        public string BootstrapServers { get; set; }
+
+        /// <summary>
+        /// Gets or sets the client group id string. All clients sharing the same group.id belong to the same group.
+        /// </summary>
+        public string GroupId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the configuration that is used to create the <see cref="Consumer{TKey, TValue}"/> for this receiver.
+        /// </summary>
+        public ConsumerConfig Config { get; set; }
+    }
+}
+#endif
