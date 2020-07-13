f48533d9779aea329a378effb5796843620b6379
Brian Friesen <brian.friesen@gmail.com>
2020-07-13 11:22:50 -0400

    Replace non-generic "To" extensions with generic To<TCloudEvent>()

--- RockLib.Messaging.CloudEvents/CloudEventExtensions.cs
+++ RockLib.Messaging.CloudEvents/CloudEventExtensions.cs
@@ -1,30 +1,48 @@
 ﻿using RockLib.Messaging.DependencyInjection;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace RockLib.Messaging.CloudEvents
 {
     /// <summary>
     /// Extension methods related to CloudEvents.
     /// </summary>
-    public static class CloudEventExtensions
+    public static partial class CloudEventExtensions
     {
-        #region CloudEvent
+        private static readonly ConcurrentDictionary<Type, Constructor> _constructors = new ConcurrentDictionary<Type, Constructor>();
 
         /// <summary>
-        /// Creates an instance of <see cref="CloudEvent"/> with properties mapped from the headers of
-        /// <paramref name="receiverMessage"/>.
+        /// Creates an instance of <typeparamref name="TCloudEvent"/> with properties mapped from
+        /// the headers of <paramref name="receiverMessage"/>.
+        /// <para>The <typeparamref name="TCloudEvent"/> type <em>must</em> have a constructor with
+        /// the exact parameters: <see cref="IReceiverMessage"/>, <see cref="IProtocolBinding"/>.
+        ///  A <see cref="MissingMemberException"/> is thrown if it does not.</para>
         /// </summary>
+        /// <typeparam name="TCloudEvent">The type of <see cref="CloudEvent"/> to create.</typeparam>
         /// <param name="receiverMessage">
-        /// The <see cref="IReceiverMessage"/> to be mapped to the new <see cref="CloudEvent"/>.
+        /// The <see cref="IReceiverMessage"/> to be mapped to the new <typeparamref name="TCloudEvent"/>.
         /// </param>
         /// <param name="protocolBinding">
         /// The <see cref="IProtocolBinding"/> used to map <see cref="IReceiverMessage"/> headers to
         /// CloudEvent attributes.
         /// </param>
         /// <returns>
-        /// A new <see cref="CloudEvent"/> with properties mapped from the headers of the <see cref="IReceiverMessage"/>.
+        /// A new <typeparamref name="TCloudEvent"/> with properties mapped from the headers of the <see cref="IReceiverMessage"/>.
         /// </returns>
-        public static CloudEvent ToCloudEvent(this IReceiverMessage receiverMessage, IProtocolBinding protocolBinding = null) =>
-            new CloudEvent(receiverMessage, protocolBinding);
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="MissingMemberException"></exception>
+        public static TCloudEvent To<TCloudEvent>(this IReceiverMessage receiverMessage, IProtocolBinding protocolBinding = null)
+            where TCloudEvent : CloudEvent
+        {
+            if (receiverMessage is null)
+                throw new ArgumentNullException(nameof(receiverMessage));
+
+            var constructor = _constructors.GetOrAdd(typeof(TCloudEvent), Constructor.Create)
+                ?? throw MissingCloudEventConstructor(typeof(TCloudEvent));
+
+            return (TCloudEvent)constructor.Invoke(receiverMessage, protocolBinding);
+        }
 
         /// <summary>
         /// Adds a <see cref="ValidatingSender"/> decorator that ensures messages are valid CloudEvents.
@@ -38,27 +56,6 @@ namespace RockLib.Messaging.CloudEvents
         public static ISenderBuilder AddCloudEventValidation(this ISenderBuilder builder, IProtocolBinding protocolBinding = null) =>
             builder.AddValidation(message => CloudEvent.Validate(message, protocolBinding));
 
-        #endregion
-
-        #region SequentialEvent
-
-        /// <summary>
-        /// Creates an instance of <see cref="SequentialEvent"/> with properties mapped from the headers of
-        /// <paramref name="receiverMessage"/>.
-        /// </summary>
-        /// <param name="receiverMessage">
-        /// The <see cref="IReceiverMessage"/> to be mapped to the new <see cref="SequentialEvent"/>.
-        /// </param>
-        /// <param name="protocolBinding">
-        /// The <see cref="IProtocolBinding"/> used to map <see cref="IReceiverMessage"/> headers to
-        /// SequentialEvent attributes.
-        /// </param>
-        /// <returns>
-        /// A new <see cref="SequentialEvent"/> with properties mapped from the headers of the <see cref="IReceiverMessage"/>.
-        /// </returns>
-        public static SequentialEvent ToSequentialEvent(this IReceiverMessage receiverMessage, IProtocolBinding protocolBinding = null) =>
-            new SequentialEvent(receiverMessage, protocolBinding);
-
         /// <summary>
         /// Adds a <see cref="ValidatingSender"/> decorator that ensures messages are valid SequentialEvents.
         /// </summary>
@@ -70,27 +67,6 @@ namespace RockLib.Messaging.CloudEvents
         /// <returns>The same <see cref="ISenderBuilder"/>.</returns>
         public static ISenderBuilder AddSequentialEventValidation(this ISenderBuilder builder, IProtocolBinding protocolBinding = null) =>
             builder.AddValidation(message => SequentialEvent.Validate(message, protocolBinding));
-
-        #endregion
-
-        #region CorrelatedEvent
-
-        /// <summary>
-        /// Creates an instance of <see cref="CorrelatedEvent"/> with properties mapped from the headers of
-        /// <paramref name="receiverMessage"/>.
-        /// </summary>
-        /// <param name="receiverMessage">
-        /// The <see cref="IReceiverMessage"/> to be mapped to the new <see cref="CorrelatedEvent"/>.
-        /// </param>
-        /// <param name="protocolBinding">
-        /// The <see cref="IProtocolBinding"/> used to map <see cref="IReceiverMessage"/> headers to
-        /// CorrelatedEvent attributes.
-        /// </param>
-        /// <returns>
-        /// A new <see cref="CorrelatedEvent"/> with properties mapped from the headers of the <see cref="IReceiverMessage"/>.
-        /// </returns>
-        public static CorrelatedEvent ToCorrelatedEvent(this IReceiverMessage receiverMessage, IProtocolBinding protocolBinding = null) =>
-            new CorrelatedEvent(receiverMessage, protocolBinding);
 
         /// <summary>
         /// Adds a <see cref="ValidatingSender"/> decorator that ensures messages are valid CorrelatedEvents.
@@ -104,6 +80,8 @@ namespace RockLib.Messaging.CloudEvents
         public static ISenderBuilder AddCorrelatedEventValidation(this ISenderBuilder builder, IProtocolBinding protocolBinding = null) =>
             builder.AddValidation(message => CorrelatedEvent.Validate(message, protocolBinding));
 
-        #endregion
+        private static MissingMemberException MissingCloudEventConstructor(Type cloudEventType) =>
+            new MissingMemberException($"CloudEvent type '{cloudEventType.Name}' must have a public constructor"
+                + $" with the exact parameters: '{nameof(IReceiverMessage)}', '{nameof(IProtocolBinding)}'.");
     }
 }
