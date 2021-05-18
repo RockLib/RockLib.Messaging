2b47ea39d207b7b381b9152797c95f69cdda3983
Brian Friesen <brianfriesen@quickenloans.com>
2021-05-18 14:08:52 -0400

    Update DI extensions for automatic reloading

--- RockLib.Messaging.NamedPipes/DependencyInjection/NamedPipeExtensions.cs
+++ RockLib.Messaging.NamedPipes/DependencyInjection/NamedPipeExtensions.cs
@@ -1,6 +1,5 @@
 ﻿#if !NET451
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using RockLib.Messaging.NamedPipes;
 using System;
 
@@ -17,17 +16,49 @@ namespace RockLib.Messaging.DependencyInjection
         /// <param name="services">The <see cref="IServiceCollection"/>.</param>
         /// <param name="name">The name of the sender.</param>
         /// <param name="configureOptions">A callback for configuring the <see cref="NamedPipeOptions"/>.</param>
+        /// <param name="reloadOnChange">
+        /// Whether to create a named pipe sender that automatically reloads itself when its
+        /// configuration or options change.
+        /// </param>
         /// <returns>A builder allowing the sender to be decorated.</returns>
-        public static ISenderBuilder AddNamedPipeSender(this IServiceCollection services, string name, Action<NamedPipeOptions> configureOptions = null)
+        public static ISenderBuilder AddNamedPipeSender(this IServiceCollection services, string name,
+            Action<NamedPipeOptions> configureOptions = null, bool reloadOnChange = true)
         {
-            return services.AddSender(serviceProvider =>
-            {
-                var optionsMonitor = serviceProvider.GetService<IOptionsMonitor<NamedPipeOptions>>();
-                var options = optionsMonitor?.Get(name) ?? new NamedPipeOptions();
-                configureOptions?.Invoke(options);
+            return services.AddSender(name, CreateNamedPipeSender, configureOptions, reloadOnChange);
 
-                return new NamedPipeSender(name, options.PipeName);
-            });
+            ISender CreateNamedPipeSender(NamedPipeOptions options, IServiceProvider serviceProvider) =>
+                new NamedPipeSender(name, options.PipeName);
+        }
+
+        /// <summary>
+        /// Adds a <see cref="NamedPipeSender"/> to the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the sender.</param>
+        /// <param name="configureOptions">A callback for configuring the <see cref="NamedPipeOptions"/>.</param>
+        /// <returns>A builder allowing the sender to be decorated.</returns>
+        public static ISenderBuilder AddNamedPipeSender(this IServiceCollection services, string name,
+            Action<NamedPipeOptions> configureOptions) =>
+            services.AddNamedPipeSender(name, configureOptions, true);
+
+        /// <summary>
+        /// Adds a <see cref="NamedPipeReceiver"/> to the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the receiver.</param>
+        /// <param name="configureOptions">A callback for configuring the <see cref="NamedPipeOptions"/>.</param>
+        /// <param name="reloadOnChange">
+        /// Whether to create a named pipe receiver that automatically reloads itself when its
+        /// configuration or options change.
+        /// </param>
+        /// <returns>A builder allowing the receiver to be decorated.</returns>
+        public static IReceiverBuilder AddNamedPipeReceiver(this IServiceCollection services, string name,
+            Action<NamedPipeOptions> configureOptions = null, bool reloadOnChange = true)
+        {
+            return services.AddReceiver(name, CreateNamedPipeReceiver, configureOptions, reloadOnChange);
+
+            IReceiver CreateNamedPipeReceiver(NamedPipeOptions options, IServiceProvider serviceProvider) =>
+                new NamedPipeReceiver(name, options.PipeName);
         }
 
         /// <summary>
@@ -37,17 +68,9 @@ namespace RockLib.Messaging.DependencyInjection
         /// <param name="name">The name of the receiver.</param>
         /// <param name="configureOptions">A callback for configuring the <see cref="NamedPipeOptions"/>.</param>
         /// <returns>A builder allowing the receiver to be decorated.</returns>
-        public static IReceiverBuilder AddNamedPipeReceiver(this IServiceCollection services, string name, Action<NamedPipeOptions> configureOptions = null)
-        {
-            return services.AddReceiver(serviceProvider =>
-            {
-                var optionsMonitor = serviceProvider.GetService<IOptionsMonitor<NamedPipeOptions>>();
-                var options = optionsMonitor?.Get(name) ?? new NamedPipeOptions();
-                configureOptions?.Invoke(options);
-
-                return new NamedPipeReceiver(name, options.PipeName);
-            });
-        }
+        public static IReceiverBuilder AddNamedPipeReceiver(this IServiceCollection services, string name,
+            Action<NamedPipeOptions> configureOptions) =>
+            services.AddNamedPipeReceiver(name, configureOptions, true);
     }
 }
 #endif
--- RockLib.Messaging.SQS/DependencyInjection/SQSExtensions.cs
+++ RockLib.Messaging.SQS/DependencyInjection/SQSExtensions.cs
@@ -2,7 +2,6 @@
 using Amazon;
 using Amazon.SQS;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using RockLib.Messaging.SQS;
 using System;
 
@@ -19,22 +18,64 @@ namespace RockLib.Messaging.DependencyInjection
         /// <param name="services">The <see cref="IServiceCollection"/>.</param>
         /// <param name="name">The name of the sender.</param>
         /// <param name="configureOptions">A callback for configuring the <see cref="SQSSenderOptions"/>.</param>
+        /// <param name="reloadOnChange">
+        /// Whether to create a named pipe sender that automatically reloads itself when its
+        /// configuration or options change.
+        /// </param>
         /// <returns>A builder allowing the sender to be decorated.</returns>
-        public static ISenderBuilder AddSQSSender(this IServiceCollection services, string name, Action<SQSSenderOptions> configureOptions = null)
+        public static ISenderBuilder AddSQSSender(this IServiceCollection services, string name,
+            Action<SQSSenderOptions> configureOptions = null, bool reloadOnChange = true)
         {
-            return services.AddSender(serviceProvider =>
-            {
-                var optionsMonitor = serviceProvider.GetService<IOptionsMonitor<SQSSenderOptions>>();
-                var options = optionsMonitor?.Get(name) ?? new SQSSenderOptions();
-                configureOptions?.Invoke(options);
+            return services.AddSender(name, CreateSQSSender, configureOptions, reloadOnChange);
 
+            ISender CreateSQSSender(SQSSenderOptions options, IServiceProvider serviceProvider)
+            {
                 var sqsClient = options.SqsClient
                     ?? (options.Region != null
                         ? new AmazonSQSClient(RegionEndpoint.GetBySystemName(options.Region))
                         : serviceProvider.GetService<IAmazonSQS>() ?? new AmazonSQSClient());
 
                 return new SQSSender(sqsClient, name, options.QueueUrl, options.MessageGroupId);
-            });
+            }
+        }
+
+        /// <summary>
+        /// Adds a <see cref="SQSSender"/> to the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the sender.</param>
+        /// <param name="configureOptions">A callback for configuring the <see cref="SQSSenderOptions"/>.</param>
+        /// <returns>A builder allowing the sender to be decorated.</returns>
+        public static ISenderBuilder AddSQSSender(this IServiceCollection services, string name,
+            Action<SQSSenderOptions> configureOptions) =>
+            services.AddSQSSender(name, configureOptions, true);
+
+        /// <summary>
+        /// Adds a <see cref="SQSReceiver"/> to the service collection.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="name">The name of the receiver.</param>
+        /// <param name="configureOptions">A callback for configuring the <see cref="SQSReceiverOptions"/>.</param>
+        /// <param name="reloadOnChange">
+        /// Whether to create a named pipe sender that automatically reloads itself when its
+        /// configuration or options change.
+        /// </param>
+        /// <returns>A builder allowing the receiver to be decorated.</returns>
+        public static IReceiverBuilder AddSQSReceiver(this IServiceCollection services, string name,
+            Action<SQSReceiverOptions> configureOptions = null, bool reloadOnChange = true)
+        {
+            return services.AddReceiver(name, CreateSQSReceiver, configureOptions, reloadOnChange);
+
+            IReceiver CreateSQSReceiver(SQSReceiverOptions options, IServiceProvider serviceProvider)
+            {
+                var sqsClient = options.SqsClient
+                    ?? (options.Region != null
+                        ? new AmazonSQSClient(RegionEndpoint.GetBySystemName(options.Region))
+                        : serviceProvider.GetService<IAmazonSQS>() ?? new AmazonSQSClient());
+
+                return new SQSReceiver(sqsClient, name, options.QueueUrl, options.MaxMessages,
+                    options.AutoAcknowledge, options.WaitTimeSeconds, options.UnpackSNS);
+            }
         }
 
         /// <summary>
@@ -44,23 +85,9 @@ namespace RockLib.Messaging.DependencyInjection
         /// <param name="name">The name of the receiver.</param>
         /// <param name="configureOptions">A callback for configuring the <see cref="SQSReceiverOptions"/>.</param>
         /// <returns>A builder allowing the receiver to be decorated.</returns>
-        public static IReceiverBuilder AddSQSReceiver(this IServiceCollection services, string name, Action<SQSReceiverOptions> configureOptions = null)
-        {
-            return services.AddReceiver(serviceProvider =>
-            {
-                var optionsMonitor = serviceProvider.GetService<IOptionsMonitor<SQSReceiverOptions>>();
-                var options = optionsMonitor?.Get(name) ?? new SQSReceiverOptions();
-                configureOptions?.Invoke(options);
-
-                var sqsClient = options.SqsClient
-                    ?? (options.Region != null
-                        ? new AmazonSQSClient(RegionEndpoint.GetBySystemName(options.Region))
-                        : serviceProvider.GetService<IAmazonSQS>() ?? new AmazonSQSClient());
-
-                return new SQSReceiver(sqsClient, name, options.QueueUrl, options.MaxMessages,
-                    options.AutoAcknowledge, options.WaitTimeSeconds, options.UnpackSNS);
-            });
-        }
+        public static IReceiverBuilder AddSQSReceiver(this IServiceCollection services, string name,
+            Action<SQSReceiverOptions> configureOptions) =>
+            services.AddSQSReceiver(name, configureOptions, true);
     }
 }
 #endif
