9ca1623cf1d4fea62f7902c9e51c4979fb4e820f
Strupat, Nick <NickStrupat@rocketmortgage.ca>
2023-10-04 17:28:29 -0400

    Add unit tests and augment existing tests that should be checking message group ID (followed the same patterns in SQSSender et al).

--- Tests/RockLib.Messaging.SNS.Tests/SNSTests.cs
+++ Tests/RockLib.Messaging.SNS.Tests/SNSTests.cs
@@ -27,5 +27,61 @@ namespace RockLib.Messaging.SNS.Tests
                    && r.MessageAttributes["bar"].StringValue == "abc"),
                 It.IsAny<CancellationToken>()));
         }
+
+        [Fact]
+        public static async Task SNSSenderSetsMessageGroupIdWhenSpecifiedInHeader()
+        {
+            var mockSns = new Mock<IAmazonSimpleNotificationService>();
+
+            using (var sender = new SNSSender(mockSns.Object, "foo", "http://url.com/foo"))
+            {
+                await sender.SendAsync(
+                    new SenderMessage("Hello, world!") { Headers = { { "bar", "abc" }, { "SNS.MessageGroupId", "123" } } })
+                    .ConfigureAwait(false);
+            }
+
+            mockSns.Verify(m => m.PublishAsync(
+                It.Is<PublishRequest>(r => r.Message == "Hello, world!"
+                   && r.MessageAttributes[HeaderNames.OriginatingSystem].StringValue == "SNS"
+                   && r.MessageAttributes["bar"].StringValue == "abc"
+                   && r.MessageGroupId == "123"),
+                It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public static async Task SQSSenderSetsMessageGroupIdWhenSpecifiedInConstructor()
+        {
+            var mockSns = new Mock<IAmazonSimpleNotificationService>();
+
+            using (var sender = new SNSSender(mockSns.Object, "foo", "http://url.com/foo", "abc"))
+            {
+                await sender.SendAsync(
+                        new SenderMessage(""))
+                    .ConfigureAwait(false);
+            }
+
+            mockSns.Verify(m => m.PublishAsync(
+                It.Is<PublishRequest>(r => r.MessageGroupId == "abc"),
+                It.IsAny<CancellationToken>()));
+        }
+
+        [Fact]
+        public static async Task SQSSenderSetsMessageGroupIdToHeaderValueWhenSpecifiedInBothHeaderAndConstructor()
+        {
+            var mockSns = new Mock<IAmazonSimpleNotificationService>();
+
+            using (var sender = new SNSSender(mockSns.Object, "foo", "http://url.com/foo"))
+            {
+                await sender.SendAsync(
+                        new SenderMessage("") { Headers = { { "SNS.MessageGroupId", "xyz" } } })
+                    .ConfigureAwait(false);
+            }
+
+            mockSns.Verify(m => m.PublishAsync(
+                It.Is<PublishRequest>(r =>
+                    !r.MessageAttributes.ContainsKey("SNS.MessageGroupId")
+                    && r.MessageGroupId == "xyz"),
+                It.IsAny<CancellationToken>()));
+        }
     }
 }
